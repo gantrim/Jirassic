b6b3c3825e269e272f030960e48deef8015c54f3
Greg Antrim <g.antrim@kunzleigh.com>
2015-10-06 08:02:19 -0400

    converted to swift 2.0

--- Vendor/RCLog.swift
+++ Vendor/RCLog.swift
@@ -9,38 +9,38 @@ import Foundation
 import CoreGraphics
 
 func RCLog (message: String, file: String = __FILE__, line: Int = __LINE__) {
-	print("\((file as NSString).lastPathComponent):\(line): \(message)")
+	print("\((file as NSString).lastPathComponent):\(line): \(message)", terminator: "")
 }
 
 func RCLogO (message: AnyObject?, file: String = __FILE__, line: Int = __LINE__) {
-	print("\((file as NSString).lastPathComponent):\(line): \(message)")
+	print("\((file as NSString).lastPathComponent):\(line): \(message)", terminator: "")
 }
 
 func RCLogI (message: Int?, file: String = __FILE__, line: Int = __LINE__) {
-	print("\((file as NSString).lastPathComponent):\(line): \(message)")
+	print("\((file as NSString).lastPathComponent):\(line): \(message)", terminator: "")
 }
 
 func RCLogF (message: Float?, file: String = __FILE__, line: Int = __LINE__) {
-	print("\((file as NSString).lastPathComponent):\(line): \(message)")
+	print("\((file as NSString).lastPathComponent):\(line): \(message)", terminator: "")
 }
 
 func RCLogRect (message: CGRect?, file: String = __FILE__, line: Int = __LINE__) {
 	if message != nil {
-		print("\((file as NSString).lastPathComponent):\(line): CGRect\(message!)")
+		print("\((file as NSString).lastPathComponent):\(line): CGRect\(message!)", terminator: "")
 	} else {
-		print("\((file as NSString).lastPathComponent):\(line): \(message)")
+		print("\((file as NSString).lastPathComponent):\(line): \(message)", terminator: "")
 	}
 }
 
 func RCLogSize (message: CGSize?, file: String = __FILE__, line: Int = __LINE__) {
-	print("\((file as NSString).lastPathComponent):\(line): \(message)")
+	print("\((file as NSString).lastPathComponent):\(line): \(message)", terminator: "")
 }
 
 func RCLogPoint (message: CGPoint?, file: String = __FILE__, line: Int = __LINE__) {
-	print("\((file as NSString).lastPathComponent):\(line): \(message)")
+	print("\((file as NSString).lastPathComponent):\(line): \(message)", terminator: "")
 }
 
 func RCLogThread (file: String = __FILE__, line: Int = __LINE__) {
 	let message = NSThread.isMainThread() ? "Log from Main Thread" : "Log from Secondary Thread"
-	print("\((file as NSString).lastPathComponent):\(line): \(message)")
+	print("\((file as NSString).lastPathComponent):\(line): \(message)", terminator: "")
 }
